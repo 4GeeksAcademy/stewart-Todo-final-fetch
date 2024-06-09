62d435bb006259e9384390f0936d439bbf302a6c
stewybenz95 <113649226+stewybenz95@users.noreply.github.com>
2024-06-09 23:55:34 +0000

    final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,28 +1,57 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect} from "react";
+
 
 //create your first component
 const Home = () => {
 	const[input, setInput] = useState("") 
 	const [todos, setTodos] = useState([])
-	function addTodo(){
-		let task = {label: input, done: false}
-		setTodos([...todos, task]);
+	useEffect(async()=> {
+		let response = await fetch("https://playground.4geeks.com/todo/users/stewart")
+		let data= response.json
+		if(data.name=="stewart"){
+			setTodos(data.todos)
+		}
+		else if(data.detail.length != 0){
+			createUser()
+		}
+	}, [])
+	async function createUser(){
+		let response = await fetch("https://playground.4geeks.com/todo/users/stewart", {
+			headers: {"Content-Type":"application/json"},
+			method: "POST"
+		})
+	}
+	async function addTodo(){
+		let task = {label: input, is_done: false}
+		let response = await fetch("https://playground.4geeks.com/todo/todos/stewart",{
+			method: "POST",
+			headers: {"Content-Type":"application/json"},
+			body: JSON.stringify(task)
+		})
+		let data = await response.json()
+		setTodos([...todos, data]);
 		setInput("");
 	}
-	function deleteTodo(i){
-		let newTodos=todos.filter((todo,index)=>index !== i)
+	async function deleteTodo(id){
+		let response = await fetch("https://playground.4geeks.com/todo/todos/" + id, {
+			method: "DELETE",
+			headers: {"Content-Type":"application/json"},
+		})
+		
+		
+		let newTodos=todos.filter((todo,index)=>todo.id !== id)
 		setTodos(newTodos)
 		console.log(todos)
 	}
 	return (
 		<div className="text-center">
-			<input onChange={(e)=>setInput(e.target.value)}></input>
+			<input onChange={(e)=>setInput(e.target.value)} value={input}></input>
 			<button onClick={()=> addTodo()}>Add Todo</button>
 			<div>
 				{todos.map((todo, index) => (
                     <div key={index}>{todo.label}
-					<button onClick={() => deleteTodo(index)}>Delete</button>
+					<button onClick={() => deleteTodo(todo.id)}>Delete</button>
 					</div>
 					
                 ))}
